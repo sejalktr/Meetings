39adcc415852d35ecd27e815d8d5dcc5adf02958
sejalktr <sejalkataria25@gmail.com>
2026-02-14 22:33:49 +0530

    Update layout.tsx
    added page scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-50 min-h-screen overflow-y-auto">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
