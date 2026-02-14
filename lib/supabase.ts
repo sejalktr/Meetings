f7752dc9d6582f1d1f2da37b7c6d024485b919bb
sejalktr <sejalkataria25@gmail.com>
2026-02-14 22:25:31 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.https://supabase.com/dashboard/project/uwzhqurqzqsooybixqny/settings/api-keys!;
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3emhxdXJxenFzb295Yml4cW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4OTM4MDEsImV4cCI6MjA4NjQ2OTgwMX0.sRDGUFir6G2fXD5pGf8FEEQP-_0BtNGLflbAZ-tNQZw!;
+// Do not paste your actual URL here. 
+// This code tells the app to pull the keys from your Vercel Environment Variables.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
